9cee73e6dd5ec7da90605a10913a57ae42e0fcb5
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-09-04 02:56:43 -0400

    dep

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 // router.get("*", function(req, res) {
-  
 router.use(function(req, res) {
-  res.sendFile(express.static(path.join(__dirname, "../client/build/index.html")));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
